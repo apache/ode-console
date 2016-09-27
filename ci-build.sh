a3c567d26f5689799fae16a5af1e15bb1acc1d52
sathwik <sathwik@apache.org>
2016-09-27 17:26:33 +0530

    Added a pom file for build on CI using docker

--- ci-build.sh
+++ ci-build.sh
@@ -18,21 +18,38 @@
 docker pull sathwik/docker-maven-gulp:latest
 
 export JAVA_OPTS="-Xmx1024M -XX:MaxPermSize=512M"
-GULP_ARGS="$@"
+MVN_ARGS="$@"
 CONTAINER_USERNAME="dummy"
 CONTAINER_GROUPNAME="dummy"
 HOMEDIR="/home/$CONTAINER_USERNAME"
 GROUP_ID=$(id -g)
 USER_ID=$( id -u)
 
-CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR"
+CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME \
+    && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME \
+    && mkdir --parent $HOMEDIR \
+    && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR"
 
 SU_USER="su $CONTAINER_USERNAME -c "
 
 NPM_COMMAND="$SU_USER 'npm install'"
- 
-GULP_COMMAND="$SU_USER 'gulp $GULP_ARGS'"
 
-FINAL_COMMAND="$CREATE_USER_COMMAND && $NPM_COMMAND && $GULP_COMMAND"
+# A hack for frontend_maven_plugin to run gulp build task command
+FRONTEND_RM_COMMAND="$SU_USER 'rm -rf /workspace/node'"
+FRONTEND_MK_COMMAND="$SU_USER 'mkdir /workspace/node && ln -s /opt/node/bin/node /workspace/node/node'"
 
-docker run --rm -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2 -v /tmp:/tmp --entrypoint bash sathwik/maven_gulp:3.9_4.5 -c "$FINAL_COMMAND"
+MVN_COMMAND="$SU_USER 'mvn $MVN_ARGS'"
+
+FINAL_COMMAND="$CREATE_USER_COMMAND \
+                && $NPM_COMMAND \
+                && $FRONTEND_RM_COMMAND \
+                && $FRONTEND_MK_COMMAND \
+                && $MVN_COMMAND \
+                && $FRONTEND_RM_COMMAND"
+
+docker run --rm \
+    -e JAVA_OPTS \
+    -v `pwd`:/workspace \
+    -v $HOME/.m2:/home/dummy/.m2 \
+    -v /tmp:/tmp \
+    --entrypoint bash sathwik/docker-maven-gulp:latest -c "$FINAL_COMMAND"
